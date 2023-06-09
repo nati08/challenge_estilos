38aedbbadd162757f0be133629cdd991d2eaf722
nati08 <natcatnote@gmail.com>
2023-06-09 18:45:16 -0300

    proyect initial setup

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Cracked from '@/components/Animation/Cracked';
+
+import './Landing.css';
+
+const Landing = () => <Cracked>challenge_estilos</Cracked>;
+
+export default Landing;
